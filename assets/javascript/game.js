151bd5254850e9d9177243f0a641effc7cb94081
jstudenski <jstudenski@gmail.com>
2017-12-12 16:32:16 -0600

    css update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,14 @@ window.onload = function() {
 
 
 var characters = [    
-  {"name":"Pikachu", "hp":100, "ap":10, "cap":50, "image":"", "attack":"bolt"},    
-  {"name":"Squirtle", "hp":120, "ap":8, "cap":25, "image":"", "attack":"tint"},  
-  {"name":"Charmander", "hp":130, "ap":7, "cap":20, "image":"", "attack":"fire"},    
-  {"name":"Bulbasaur", "hp":150, "ap":5, "cap":10, "image":"", "attack":"leaf"}
+  {"name":"Pikachu", "hp":100, "ap":10, "cap":50, "image":"", "bg":"#ffeb3b", "attack":"bolt"},    
+  {"name":"Squirtle", "hp":120, "ap":8, "cap":25, "image":"", "bg":"#488cc7", "attack":"tint"},  
+  {"name":"Charmander", "hp":130, "ap":7, "cap":20, "image":"", "bg":"#ff9b00", "attack":"fire"},    
+  {"name":"Bulbasaur", "hp":150, "ap":5, "cap":10, "image":"", "bg":"#05ab64", "attack":"leaf"}
 ];
 var characterIndex;
 var defenderIndex;
+
 
 
 
@@ -24,6 +25,7 @@ $.each( characters, function( key, value ) {
   $div.html('<h3>' + characters[key].name + '</h3>');
   $div.append('<div class="container"><div class="charimg"><img src="assets/images/' + characters[key].name + '.gif"></div></div>');
 
+
   $div.append('<div class="hp-area"></div>');
   $div.append('<div class="life-bar"><div></div></div>');
   $("#characters").append($div);
@@ -32,11 +34,16 @@ $.each( characters, function( key, value ) {
 });
 
 
+
+
+
 $(".character").click(function() {
 
   // pick a character
   if (characterIndex === undefined) {
     characterIndex = $(this).attr('number');
+    $("[number='" + characterIndex + "'] .container").css("background-color", characters[characterIndex].bg);
+
     // Set player character base attack power
     characters[characterIndex].baseAP = characters[characterIndex].ap;
 
@@ -67,6 +74,8 @@ $(".character").click(function() {
       return
     }
     defenderIndex = $(this).attr('number');
+    $("[number='" + defenderIndex + "'] .container").css("background-color", characters[defenderIndex].bg);
+    
     $(this).appendTo("#defender");
     $("#notification").html("Click attack to fight!");
     $("#battle-update h4").html(characters[characterIndex].name + " -vs- " + characters[defenderIndex].name);
@@ -110,7 +119,7 @@ $("#attack").click(function() {
   } 
   // shake if hit
   $("[number='" + characterIndex + "'] .charimg").effect( "shake", {times:2, distance: 2}, 400 );
-
+  $("[number='" + defenderIndex + "'] .charimg").effect( "shake", {times:2, distance: 2}, 400 );
 
   // take counter attack
   characters[characterIndex].health -= characters[defenderIndex].cap;
@@ -142,28 +151,33 @@ function updateLife(number) {
   var $element = $("[number='" + number + "'] .life-bar");
   // set width of life-bar
   var progressBarWidth = percentHealth * $element.width()/100;
+
   $element.find('div').animate({ width: progressBarWidth }, 300);
-  // color accordingly
-  if (percentHealth === 100) {
-    bgColor("#4caf50")
-  } else if (percentHealth > 60) {
-    $element.find('div').css("background-color", "#8bc34a"); // lime
-  } else if (percentHealth > 40) {
-    $element.find('div').css("background-color", "#ffeb3b"); // yellow
-  } else if (percentHealth > 30) {
-    $element.find('div').css("background-color", "#ffc107"); // light orange
-  } else if (percentHealth > 20) {
-    $element.find('div').css("background-color", "#ff9800"); // orange
-  } else if (percentHealth > 10) {
-    $element.find('div').css("background-color", "#ff5722"); // tomato
-  } else {
-    $element.find('div').css("background-color", "#f44336"); // red
-  }
-
-
+  
   function bgColor(color) {
     $element.find('div').css("background-color", color); // green
   }
+
+  // color accordingly
+  if (percentHealth === 100) {
+    bgColor("#4caf50"); // green
+  } else if (percentHealth > 60) {
+    bgColor("#8bc34a"); // lime
+  } else if (percentHealth > 40) {
+    bgColor("#ffeb3b"); // yellow
+  } else if (percentHealth > 30) {
+    bgColor("#ffc107"); // light orange
+  } else if (percentHealth > 20) {
+    bgColor("#ff9800"); // orange
+  } else if (percentHealth > 10) {
+    bgColor("#ff5722"); // tomato
+  } else {
+    bgColor("#f44336"); // red
+  }
+
+
+
+
 }
 
 
